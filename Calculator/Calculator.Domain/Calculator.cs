40755bf059d5194bc94e1abd1110b806e203724b
Brunoenr02 <brunoancco02@gmail.com>
2025-05-24 18:04:46 -0500

    Actividad01

--- Calculator/Calculator.Domain/Calculator.cs
+++ Calculator/Calculator.Domain/Calculator.cs
@@ -4,4 +4,6 @@ public class Calculator
 {
     public int Add(int x, int y) => x + y;
     public int Subtract(int x, int y) => x - y;
+    public int Multiply(int x, int y) => x * y;
+    public int Divide(int x, int y) => x / y;
 }
